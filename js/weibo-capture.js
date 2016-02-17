a7680238558d87346af0e8ffa74794d47130a141
maq128 <maq128@126.com>
2016-02-17 09:50:46 +0800

    .

--- js/weibo-capture.js
+++ js/weibo-capture.js
@@ -4,10 +4,12 @@
 	进入新浪微博某个博主的微博列表页面，把下面的代码复制粘贴到 console 并运行。
 	点击页面左上角的“抓”按钮可以提取当前页面中的微博内容。
 	点击“保存”按钮可以把已经提取出的内容保存到文件。
+
 	程序会自动向下滚屏直至列表显示完整，提取完当前页后会自动翻到下一页。
 */
 (function() {
 
+// 把文本内容保存到文件里
 function saveToFile(str, filename, mime)
 {
 	var blob = new window.Blob([str], {type: mime});
@@ -26,6 +28,7 @@ function saveToFile(str, filename, mime)
 	}, 66);
 }
 
+// 从一个给定的 DOM 元素中按照微博格式提取出其中的文本内容
 function extractWeiboText(div)
 {
 	var text = '';
@@ -51,8 +54,10 @@ function extractWeiboText(div)
 
 var results = [];
 
+// 提取当前页面中的微博内容
 function capture()
 {
+	// 遍历列表项
 	var details = document.getElementsByClassName('WB_detail');
 	for (var i=0; i < details.length; i++) {
 		var detail = details[i];
@@ -60,14 +65,16 @@ function capture()
 		var client = '-';
 		var text = '-';
 		var refer = null;
-		
+
 		for (var j=0; j < detail.children.length; j++) {
 			var div = detail.children[j];
 
+			// 本条微博正文
 			if (div.className == 'WB_text W_f14') {
 				text = extractWeiboText(div);
 			}
 
+			// 被转发的原微博
 			if (div.className == 'WB_feed_expand') {
 				var expand = div.getElementsByClassName('WB_expand')[0];
 				var info = expand.getElementsByClassName('WB_info')[0];
@@ -75,10 +82,12 @@ function capture()
 					refer = info.firstElementChild.innerText.trim();
 					refer += '\t|\t' + extractWeiboText(expand.getElementsByClassName('WB_text')[0]);
 				} else {
+					// 原微博已被删除
 					refer = '-\t|\t' + expand.innerText.trim();
 				}
 			}
 
+			// 本条微博的发布时间及客户端
 			if (div.className == 'WB_from S_txt2') {
 				var infos = div.getElementsByTagName('A');
 				time = infos[0].innerText.trim();
@@ -96,6 +105,7 @@ function capture()
 	}
 	btnSave.innerHTML = '存(' + results.length + ')';
 
+	// 自动翻到下一页
 	var btnNext = document.getElementsByClassName('page next')[0]
 	if (btnNext) {
 		btnNext.click();
@@ -115,6 +125,7 @@ function save()
 	saveToFile(results.join('\r\n'), 'weibo-capture.txt', 'text/plain');
 }
 
+// 创建三个操作按钮
 var divBoard = document.createElement('div');
 divBoard.style.position = 'fixed';
 divBoard.style.zIndex = 10000;
@@ -143,6 +154,7 @@ btnSave.innerHTML = '存(0)';
 btnSave.style.cursor = 'pointer';
 btnSave.addEventListener('click', save);
 
+// 强制滚屏到底，显示当前页的完整列表
 setInterval(function() {
 	window.scrollTo(0, 100000);
 }, 500);
