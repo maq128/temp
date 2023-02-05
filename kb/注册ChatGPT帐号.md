03389bf58bb2d9793c3cc587d235488177991bda
maq128 <maq128@126.com>
2023-02-05 12:03:41 +0800

    -

---
+++ kb/注册ChatGPT帐号.md
@@ -0,0 +1,20 @@
+# 注册方法
+
+![注册ChatGPT帐号](./%E6%B3%A8%E5%86%8CChatGPT%E5%B8%90%E5%8F%B7.png)
+Edit by FreeMind
+
+# 参考资料
+
+[[保姆级教程] 国内如何注册使用 ChatGPT](https://zblogs.top/how-to-register-openai-chatgpt-in-china/)
+
+[注册ChatGPT全攻略](https://mirror.xyz/boxchen.eth/9O9CSqyKDj4BKUIil7NC1Sa1LJM-3hsPqaeW_QjfFBc)
+
+[2022 最全谷歌 Gmail 邮箱注册方法，解决手机无法验证等难题](https://pandavpnpro.com/blog/zh-cn/gmail-sign-up)
+```
+划重点：
+一、连接稳定、能提供干净上网环境的 VPN；
+二、将手机或者浏览器语言改成英语；
+三、填写手机号时不要选择国家，手动输入“+国际区号”+“空格”+“手机号码”。
+```
+
+[接码平台 SMS-ACTIVATE](https://sms-activate.org/)
---
+++ kb/注册ChatGPT帐号.mm
@@ -0,0 +1,39 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node BACKGROUND_COLOR="#1fe909" CREATED="1675527484346" ID="ID_692241682" MODIFIED="1675567140984" TEXT="&#x6ce8;&#x518c;ChatGPT&#x5e10;&#x53f7;">
+<node BACKGROUND_COLOR="#b3261e" COLOR="#ffffff" CREATED="1675564552253" ID="ID_789570238" MODIFIED="1675566636576" POSITION="left" TEXT="&#x6ce8;&#x518c;Gmail&#x90ae;&#x7bb1;&#x5e10;&#x53f7;">
+<node BACKGROUND_COLOR="#b3261e" COLOR="#ffffff" CREATED="1675564670717" ID="ID_410628180" MODIFIED="1675566636577" TEXT="&#x53c2;&#x8003; https://pandavpnpro.com/blog/zh-cn/gmail-sign-up"/>
+<node BACKGROUND_COLOR="#b3261e" COLOR="#ffffff" CREATED="1675527893223" ID="ID_1489099701" MODIFIED="1675566636578" TEXT="0.1. &#x901a;&#x8fc7;&#x4ee3;&#x7406;&#x4f7f;&#x7528;&#x5883;&#x5916;IP&#x5730;&#x5740;&#xff08;&#x9999;&#x6e2f;&#x4e0d;&#x884c;&#xff09;"/>
+<node BACKGROUND_COLOR="#b3261e" COLOR="#ffffff" CREATED="1675527813667" ID="ID_1400585506" MODIFIED="1675566636577" TEXT="0.2. &#x6d4f;&#x89c8;&#x5668;&#x8bed;&#x8a00;&#x8bbe;&#x7f6e;&#x4e3a;&#x82f1;&#x8bed;"/>
+<node BACKGROUND_COLOR="#b3261e" COLOR="#ffffff" CREATED="1675567091029" ID="ID_1676929712" MODIFIED="1675567138940" STYLE="bubble" TEXT="1. &#x6253;&#x5f00; https://gmail.com/">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+</node>
+<node BACKGROUND_COLOR="#b3261e" COLOR="#ffffff" CREATED="1675565623068" ID="ID_966415997" MODIFIED="1675567145425" TEXT="2. &#x8bbe;&#x7f6e;&#x8d26;&#x53f7;&#x540d;&#x3001;&#x5bc6;&#x7801;"/>
+<node BACKGROUND_COLOR="#b3261e" COLOR="#ffffff" CREATED="1675565661212" ID="ID_1545330025" MODIFIED="1675567148865" TEXT="3. &#x624b;&#x673a;&#x9a8c;&#x8bc1;&#x7801;&#xff08;&#x53ef;&#x4ee5;&#x4f7f;&#x7528;&#x5883;&#x5185;&#x624b;&#x673a;&#x53f7;&#xff09;"/>
+</node>
+<node BACKGROUND_COLOR="#202123" COLOR="#ffffff" CREATED="1675527720320" ID="ID_218491661" MODIFIED="1675566896603" POSITION="right" TEXT="&#x6ce8;&#x518c; OpenAI &#x5e10;&#x53f7;">
+<node BACKGROUND_COLOR="#202123" COLOR="#ffffff" CREATED="1675527893223" ID="ID_853405545" MODIFIED="1675566896605" TEXT="0. &#x901a;&#x8fc7;&#x4ee3;&#x7406;&#x4f7f;&#x7528;&#x5883;&#x5916;IP&#x5730;&#x5740;&#xff08;&#x9999;&#x6e2f;&#x4e0d;&#x884c;&#xff09;"/>
+<node BACKGROUND_COLOR="#202123" COLOR="#ffffff" CREATED="1675527564572" ID="ID_1157536308" MODIFIED="1675566896606" TEXT="1. &#x6253;&#x5f00;https://chat.openai.com/chat"/>
+<node BACKGROUND_COLOR="#202123" COLOR="#ffffff" CREATED="1675528239802" ID="ID_1334054973" MODIFIED="1675568295389" TEXT="2. &#x4f7f;&#x7528;&#x90ae;&#x7bb1;&#x4f5c;&#x4e3a;&#x6ce8;&#x518c;&#x5e10;&#x53f7;&#x540d;&#xa;&#x53ef;&#x4ee5;&#x4f7f;&#x7528;&#x666e;&#x901a;&#x7684;&#x7535;&#x5b50;&#x90ae;&#x7bb1;&#xff08;&#x5305;&#x62ec;&#x5883;&#x5185;&#x7684;&#x7535;&#x5b50;&#x90ae;&#x7bb1;&#x670d;&#x52a1;&#xff09;&#xa;&#x4e5f;&#x53ef;&#x4ee5;&#x4f7f;&#x7528;Google&#x6216;&#x8005;Outlook&#x4fe1;&#x4efb;&#x767b;&#x5f55;"/>
+<node BACKGROUND_COLOR="#202123" COLOR="#ffffff" CREATED="1675564211176" ID="ID_1188491820" MODIFIED="1675566896606" TEXT="3. &#x5728;&#x90ae;&#x7bb1;&#x4e2d;&#x63a5;&#x6536;&#x6fc0;&#x6d3b;&#x94fe;&#x63a5;&#xff0c;&#x5e76;&#x5728;&#x6d4f;&#x89c8;&#x5668;&#x4e2d;&#x6253;&#x5f00;&#x6fc0;&#x6d3b;"/>
+<node BACKGROUND_COLOR="#202123" COLOR="#ffffff" CREATED="1675527743214" ID="ID_342153129" MODIFIED="1675566896606" TEXT="4. &#x624b;&#x673a;&#x9a8c;&#x8bc1;&#x7801;">
+<node BACKGROUND_COLOR="#202123" COLOR="#ffffff" CREATED="1675565172279" ID="ID_1295599724" MODIFIED="1675568540886" TEXT="&#x5728;&#x63a5;&#x7801;&#x5e73;&#x53f0;&#x9009;&#x62e9;OpenAI&#x670d;&#x52a1;&#x4ee5;&#x53ca;&#x9002;&#x5f53;&#x7684;&#x56fd;&#x5bb6;&#xa;&#x9700;&#x6ce8;&#x610f;&#x652f;&#x6301;&#x7684;&#x5730;&#x533a;&#x8303;&#x56f4;&#xff0c;&#x4e0d;&#x662f;&#x6240;&#x6709;&#x5217;&#x51fa;&#x6765;&#x7684;&#x90fd;&#x652f;&#x6301;"/>
+<node BACKGROUND_COLOR="#202123" COLOR="#ffffff" CREATED="1675565317355" ID="ID_1392341732" MODIFIED="1675568527708" TEXT="&#x8d2d;&#x4e70;&#x670d;&#x52a1;&#xff0c;&#x5f97;&#x5230;&#x624b;&#x673a;&#x53f7;&#x7801;"/>
+<node BACKGROUND_COLOR="#202123" COLOR="#ffffff" CREATED="1675565340508" ID="ID_1716607720" MODIFIED="1675566896604" TEXT="&#x5728;&#x6ce8;&#x518c;&#x9875;&#x9762;&#x8f93;&#x5165;&#x624b;&#x673a;&#x53f7;&#x7801;&#xff0c;&#x7b49;&#x5f85;&#x9a8c;&#x8bc1;&#x7801;"/>
+<node BACKGROUND_COLOR="#202123" COLOR="#ffffff" CREATED="1675565374500" ID="ID_86243200" MODIFIED="1675566896606" TEXT="&#x5728;&#x63a5;&#x7801;&#x5e73;&#x53f0;&#x6536;&#x5230;&#x9a8c;&#x8bc1;&#x7801;&#xff0c;&#x5728;&#x6ce8;&#x518c;&#x9875;&#x9762;&#x8f93;&#x5165;"/>
+</node>
+</node>
+<node BACKGROUND_COLOR="#254d71" COLOR="#ffffff" CREATED="1675564882995" ID="ID_923370098" MODIFIED="1675566547689" POSITION="left" TEXT="&#x5883;&#x5916;&#x624b;&#x673a;&#x53f7;&#x63a5;&#x6536;&#x9a8c;&#x8bc1;&#x7801;">
+<node BACKGROUND_COLOR="#254d71" COLOR="#ffffff" CREATED="1675565867018" ID="ID_1408673421" MODIFIED="1675567270353" TEXT="1. &#x6253;&#x5f00; https://sms-activate.org/"/>
+<node BACKGROUND_COLOR="#254d71" COLOR="#ffffff" CREATED="1675565951525" ID="ID_1849494520" MODIFIED="1675567327134" TEXT="2. &#x4f7f;&#x7528;&#x90ae;&#x7bb1;&#x4f5c;&#x4e3a;&#x6ce8;&#x518c;&#x5e10;&#x53f7;&#x540d;&#xa;&#x5883;&#x5185;&#x7535;&#x5b50;&#x90ae;&#x7bb1;&#x670d;&#x52a1;&#x53ef;&#x80fd;&#x65e0;&#x6cd5;&#x6536;&#x5230;&#x6fc0;&#x6d3b;&#x94fe;&#x63a5;"/>
+<node BACKGROUND_COLOR="#254d71" COLOR="#ffffff" CREATED="1675566007837" ID="ID_1419617203" MODIFIED="1675567351794" TEXT="3. &#x5728;&#x90ae;&#x7bb1;&#x4e2d;&#x63a5;&#x6536;&#x6fc0;&#x6d3b;&#x94fe;&#x63a5;&#xff0c;&#x5e76;&#x5728;&#x6d4f;&#x89c8;&#x5668;&#x4e2d;&#x6253;&#x5f00;&#x6fc0;&#x6d3b;"/>
+<node BACKGROUND_COLOR="#254d71" COLOR="#ffffff" CREATED="1675566040588" ID="ID_448001152" MODIFIED="1675567968841" TEXT="4. &#x5145;&#x503c;&#xa;&#x53ef;&#x4f7f;&#x7528;&#x652f;&#x4ed8;&#x5b9d;&#x8fdb;&#x884c;&#x5145;&#x503c;&#xa;&#x5145;&#x503c;&#x4ee5;&#x7f8e;&#x5143;&#x8ba1;&#xff0c;&#x6d88;&#x8d39;&#x4ee5;&#x5362;&#x5e03;&#x8ba1;&#xff0c;&#x591a;&#x5145;&#x65e0;&#x76ca;"/>
+<node BACKGROUND_COLOR="#254d71" COLOR="#ffffff" CREATED="1675567415829" ID="ID_1606644323" MODIFIED="1675567534746" STYLE="bubble" TEXT="5. &#x9009;&#x62e9;&#x76f8;&#x5173;&#x7684;&#x670d;&#x52a1;&#x548c;&#x56fd;&#x5bb6;&#x5730;&#x533a;&#xff0c;&#x786e;&#x5b9a;&#x8d2d;&#x4e70;">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+</node>
+<node BACKGROUND_COLOR="#254d71" COLOR="#ffffff" CREATED="1675567460494" ID="ID_1465870366" MODIFIED="1675567488356" STYLE="bubble" TEXT="6. &#x5f97;&#x5230;&#x624b;&#x673a;&#x53f7;&#x7801;&#xff0c;&#x7b49;&#x5f85;&#x63a5;&#x6536;&#x77ed;&#x4fe1;&#x9a8c;&#x8bc1;&#x7801;">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+</node>
+</node>
+</node>
+</map>
